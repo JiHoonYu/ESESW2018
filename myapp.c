d5784643f5c3d49afa88652927c013ebff5f13d6
JH <fbwlgns0715@gmail.com>
2018-10-16 16:56:40 +0900

    add turbo_

--- myapp.c
+++ myapp.c
@@ -2,10 +2,23 @@
 #include "func2.h"
 #include <stdio.h>
 
+void turbo_on()
+{
+  printf("turbo_on\n");
+}
+void turbo_off()
+{
+  printf("turbo_ff\n");
+}
 int main()
 {
+  turbo_on();
+
   func1();
   func2();
+
+  turbo_off();
+
   return 0;
 }
 
