9812dcb66d203dadffc21e03229c00e73a515c3d
JH <fbwlgns0715@gmail.com>
2018-10-16 17:01:45 +0900

    add postprocess

--- myapp.c
+++ myapp.c
@@ -7,12 +7,20 @@ void preprocess()
   printf("do some preprocessing\n");
 }
 
+void postprocess()
+{
+  printf("do some postprocessing\n");
+}
+
 int main()
 {
   preprocess();
 
   func1();
   func2();
+
+  postprcess();
+
   return 0;
 }
 
