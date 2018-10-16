f8bf4e8dcf207f2c017a9df1a5a5b0fcee6f0069
JH <fbwlgns0715@gmail.com>
2018-10-16 16:44:29 +0900

    addition

---
+++ func2.c
@@ -0,0 +1,7 @@
+#include "func2.h"
+#include <stdio.h>
+
+void fucn2()
+{
+  printf("hello, func2\n");
+}
--- myapp.c
+++ myapp.c
@@ -1,9 +1,11 @@
 #include "func1.h"
+#include "func2.h"
 #include <stdio.h>
 
 int main()
 {
   func1();
+  func2();
   return 0;
 }
 
